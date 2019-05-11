5464ff0f7bd351c12a53f6fe68f1588079fd88b7
Dirk Gadsden <dirk@esherido.com>
2019-05-11 11:45:46 -0700

    Add plugin to auto-generate episode pages from index of episodes

---
+++ _plugins/episodes.rb
@@ -0,0 +1,24 @@
+module Jekyll
+  class EpisodePageGenerator < Generator
+    def generate(site)
+      site.data['index'].each do |feed, episodes|
+        episodes.each do |episode|
+          site.pages << EpisodePage.new(site, site.source, "#{feed}", episode)
+        end
+      end
+    end
+  end
+
+  class EpisodePage < Page
+    def initialize(site, base, dir, episode)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = "#{episode['number']}-#{Jekyll::Utils.slugify(episode['title'])}.html"
+
+      process(name)
+      read_yaml('_layouts', 'episode.html')
+      self.data['transcript'] = File.read("_data/episodes/#{episode['source']}")
+    end
+  end
+end
