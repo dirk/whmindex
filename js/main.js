240523303b892d83f58680a92bcaafb09df2fe8a
Dirk Gadsden <dirk@esherido.com>
2019-05-11 19:21:50 -0700

    Start building search functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 ;(function() {
   const WhmIndex = window.WhmIndex = {}
-  console.log('There!')
+
+  const searchInput = document.querySelector('.search-container input[type="search"]')
+  if (searchInput) {
+    const handleChange = _.debounce((event) => {
+      const value = searchInput.value.trim()
+      fetch(`/api/search.json?query=${encodeURIComponent(value)}`)
+    }, 500)
+    searchInput.addEventListener('change', handleChange)
+    searchInput.addEventListener('input', handleChange)
+  }
 })();
--- serve.go
+++ serve.go
@@ -5,9 +5,18 @@ import (
 	"net/http"
 )
 
+func handleSearch(res http.ResponseWriter, req *http.Request) {
+	query := req.FormValue("query")
+	if query == "" {
+		res.Write([]byte(`{"message":"Missing query"}`))
+	}
+	res.Write([]byte(`{}`))
+}
+
 func serve() error {
 	fileServer := http.FileServer(http.Dir("_site"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/api/search.json", handleSearch)
 
 	port := 3000
 	fmt.Printf("Listening on %v...\n", port)
