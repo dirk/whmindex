bdd84023fee3c96ab62edb120d8b49957461401b
Dirk Gadsden <dirk@esherido.com>
2019-05-16 22:25:10 -0700

    Render search results on the front-end

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@
     const handleChange = _.debounce((event) => {
       const value = searchInput.value.trim()
       fetch(`/api/search.json?query=${encodeURIComponent(value)}`)
+        .then(response => response.json())
+        .then(result => {
+          let searchResults = document.querySelector('.search-container .results')
+          if (!searchResults) {
+            searchResults = document.createElement('div')
+            searchResults.className = 'results'
+            searchInput.parentElement.appendChild(searchResults)
+          }
+          if (result.matches.length > 0) {
+            const elements = result.matches.map(match => (
+              `<li>${match.episode.title}</li>`
+            ))
+            searchResults.innerHTML = `<ul>${elements.join('')}</ul>`
+          } else {
+            searchResults.innerHTML = 'No results found.'
+          }
+        })
     }, 500)
     searchInput.addEventListener('change', handleChange)
     searchInput.addEventListener('input', handleChange)
